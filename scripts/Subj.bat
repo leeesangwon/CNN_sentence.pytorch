c29ccceaba9d342041138402fb6c0b6714e14672
Sangwon Lee <leee.sangwon@gmail.com>
2019-10-18 16:08:24 +0900

    Create cmd scripts

---
+++ scripts/Subj.bat
@@ -0,0 +1,6 @@
+cd ../src
+python main.py --dataset=Subj --dataset_folder="../resource/Subj" --model=random
+python main.py --dataset=Subj --dataset_folder="../resource/Subj" --model=static
+python main.py --dataset=Subj --dataset_folder="../resource/Subj" --model=non-static
+python main.py --dataset=Subj --dataset_folder="../resource/Subj" --model=multi-channel
+cd ../scripts
